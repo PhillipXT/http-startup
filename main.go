1ab80e01f37a4187fb2aa55059073ab4bf1bc75a
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-08-14 21:11:07 -0600

    Read a file in 8-byte chunks

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import (
     "fmt"
+    "io"
+    "log"
+    "os"
 )
 
 func main() {
-    fmt.Println("I hope I get the job!")
+    file, err := os.Open("messages.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for {
+        buffer := make([]byte, 8)
+        n, err := file.Read(buffer)
+        if err == io.EOF {
+            break
+        }
+        if n > 0 {
+            fmt.Printf("read: %s\n", string(buffer))
+        }
+    }
 }
