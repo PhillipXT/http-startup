db9e9a1aa7ace959ef59dd0ce6277b82829ce782
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-08-15 14:50:03 -0600

    Added channels for lines

--- main.go
+++ main.go
@@ -21,22 +21,37 @@ func main() {
 
     fmt.Printf("Reading data from %s:\n", filePath)
 
-    line := ""
-
-    for {
-        buffer := make([]byte, 8)
-        n, err := file.Read(buffer)
-        if err == io.EOF {
-            break
-        } else if err != nil {
-            fmt.Printf("error: %s\n", err.Error())
-            break
-        }
-        parts := strings.Split(string(buffer[:n]), "\n")
-        for i := 0; i < len(parts) - 1; i++ {
-            fmt.Printf("read: %s%s\n", line, parts[i])
-            line = ""
-        }
-        line += parts[len(parts)-1]
+    lines := getLinesChannel(file)
+    for line := range lines {
+        fmt.Println("read:", line)
     }
+
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+    ch := make(chan string)
+
+    go func() {
+        defer close(ch)
+        line := ""
+        for {
+            buffer := make([]byte, 8)
+            n, err := f.Read(buffer)
+            if err == io.EOF {
+                break
+            } else if err != nil {
+                fmt.Printf("error: %s\n", err.Error())
+                break
+            }
+            parts := strings.Split(string(buffer[:n]), "\n")
+            for i := 0; i < len(parts) - 1; i++ {
+                ch <- fmt.Sprintf("%s%s", line, parts[i])
+                line = ""
+            }
+            line += parts[len(parts)-1]
+        }
+    }()
+
+    return ch
 }
