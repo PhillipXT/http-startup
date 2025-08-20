0106c3193748a04a0fc7d451d534bd1031e5f47d
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-08-20 15:42:31 -0600

    Parse headers in the main loop

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,11 +1,11 @@
 package headers
 
 import (
-    "bytes"
-    "fmt"
-    "log"
-    "slices"
-    "strings"
+	"bytes"
+	"fmt"
+	"log"
+	"slices"
+	"strings"
 )
 
 type Headers map[string]string
@@ -14,49 +14,49 @@ const crlf = "\r\n"
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 
-    // If no CRLF is found, we don't have enough data to process yet
-    index := bytes.Index(data, []byte(crlf))
-    if index == -1 {
-        return 0, false, nil
-    }
+	// If no CRLF is found, we don't have enough data to process yet
+	index := bytes.Index(data, []byte(crlf))
+	if index == -1 {
+		return 0, false, nil
+	}
 
-    // If CRLF is found at the beginning, we are at the end of the headers
-    if index == 0 {
-        return 2, true, nil
-    }
+	// If CRLF is found at the beginning, we are at the end of the headers
+	if index == 0 {
+		return 2, true, nil
+	}
 
-    line := strings.TrimSpace(string(data[:index]))
+	line := strings.TrimSpace(string(data[:index]))
 
-    colon := strings.Index(line, ":")
-    if colon == -1 {
-        return 0, false, fmt.Errorf("Malformed header: %s", line)
-    }
+	colon := strings.Index(line, ":")
+	if colon == -1 {
+		return 0, false, fmt.Errorf("Malformed header: %s", line)
+	}
 
-    // Don't trim space in the key because we need to check for invalid
-    // spaces, i.e. "Host :", so we need the key to be "Host "
-    key := strings.ToLower(line[:colon])
-    value := strings.TrimSpace(line[colon + 1:])
+	// Don't trim space in the key because we need to check for invalid
+	// spaces, i.e. "Host :", so we need the key to be "Host "
+	key := strings.ToLower(line[:colon])
+	value := strings.TrimSpace(line[colon+1:])
 
-    log.Printf("Header line: %s\n", line)
-    log.Printf("Parsed header: [%s] == [%s]\n", key, value)
+	log.Printf("Header line: %s\n", line)
+	log.Printf("Parsed header: [%s] == [%s]\n", key, value)
 
-    validChars := []byte{'!','#','$','%','&','\'','*','+','-','.','^','_','`','|','~'}
+	validChars := []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
 
-    for _, char := range []byte(key) {
-        if (char <= 'a' || char >= 'z') && (char <= 'A' || char >= 'Z') && (char <= '0' || char >= '9') && !slices.Contains(validChars, char) {
-            return 0, false, fmt.Errorf("Header key contains illegal characters: %s", line)
-        }
-    }
+	for _, char := range []byte(key) {
+		if (char < 'a' || char > 'z') && (char < 'A' || char > 'Z') && (char < '0' || char > '9') && !slices.Contains(validChars, char) {
+			return 0, false, fmt.Errorf("Header key contains illegal characters: [%s] %s", string(char), line)
+		}
+	}
 
-    if val, ok := h[key]; ok {
-        h[key] = val + ", " + value
-    } else {
-        h[key] = value
-    }
+	if val, ok := h[key]; ok {
+		h[key] = val + ", " + value
+	} else {
+		h[key] = value
+	}
 
-    return index + 2, false, nil
+	return index + 2, false, nil
 }
 
 func NewHeaders() Headers {
-    return make(map[string]string)
+	return make(map[string]string)
 }
--- internal/request/request.go
+++ internal/request/request.go
@@ -1,146 +1,179 @@
 package request
 
 import (
-    "bytes"
-    "fmt"
-    "io"
-    "strings"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	"github.com/PhillipXT/http-startup/internal/headers"
 )
 
 type Request struct {
-    RequestLine RequestLine
-    state requestState
+	RequestLine RequestLine
+	Headers     headers.Headers
+	state       requestState
 }
 
 type RequestLine struct {
-    HttpVersion string
-    RequestTarget string
-    Method string
+	HttpVersion   string
+	RequestTarget string
+	Method        string
 }
 
 type requestState int
 
 const (
-    requestStateInitialized requestState = iota
-    requestStateDone
+	requestStateInitialized requestState = iota
+	requestStateParsingHeaders
+	requestStateDone
 )
 
 const crlf = "\r\n"
 const bufferSize = 8
 
-func RequestFromReader (reader io.Reader) (*Request, error) {
+func RequestFromReader(reader io.Reader) (*Request, error) {
 
-    buffer := make([]byte, bufferSize, bufferSize)
+	buffer := make([]byte, bufferSize)
 
-    readToIndex := 0
+	readToIndex := 0
 
-    request := &Request{
-        state: requestStateInitialized,
-    }
+	request := &Request{
+		Headers: headers.NewHeaders(),
+		state:   requestStateInitialized,
+	}
 
-    for request.state != requestStateDone {
-        if readToIndex >= len(buffer) {
-            newB := make([]byte, len(buffer) * 2)
-            copy(newB, buffer)
-            buffer = newB
-        }
+	for request.state != requestStateDone {
+		if readToIndex >= len(buffer) {
+			newB := make([]byte, len(buffer)*2)
+			copy(newB, buffer)
+			buffer = newB
+		}
 
-        bytesRead, err := reader.Read(buffer[readToIndex:])
-        if err != nil {
-            if err == io.EOF {
-                request.state = requestStateDone
-                break
-            }
-            return nil, err
-        }
+		bytesRead, err := reader.Read(buffer[readToIndex:])
+		if err != nil {
+			if err == io.EOF {
+				if request.state != requestStateDone {
+					return nil, fmt.Errorf("incomplete request:  state (%d) bytes (%d)", request.state, bytesRead)
+				}
+			}
+			return nil, err
+		}
 
-        readToIndex += bytesRead
+		readToIndex += bytesRead
 
-        bytesParsed, err := request.parse(buffer[:readToIndex])
-        if err != nil {
-            return nil, err
-        }
+		bytesParsed, err := request.parse(buffer[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
 
-        copy(buffer, buffer[bytesParsed:])
-        readToIndex -= bytesParsed
-    }
+		copy(buffer, buffer[bytesParsed:])
+		readToIndex -= bytesParsed
+	}
 
-    return request, nil
+	return request, nil
 }
 
 func parseRequestLine(data []byte) (*RequestLine, int, error) {
 
-    index := bytes.Index(data, []byte(crlf))
-    if index == -1 {
-        return nil, 0, nil
-    }
+	index := bytes.Index(data, []byte(crlf))
+	if index == -1 {
+		return nil, 0, nil
+	}
 
-    line := string(data[:index])
+	line := string(data[:index])
 
-    request_line, err := requestLineFromString(line)
-    if err != nil {
-        return nil, 0, err
-    }
+	request_line, err := requestLineFromString(line)
+	if err != nil {
+		return nil, 0, err
+	}
 
-    return request_line, index + 2, nil
+	return request_line, index + 2, nil
 }
 
 func requestLineFromString(line string) (*RequestLine, error) {
 
-    parts := strings.Split(line, " ")
+	parts := strings.Split(line, " ")
 
-    if len(parts) != 3 {
-        return nil, fmt.Errorf("Malformed request line: %si", line)
-    }
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("Malformed request line: %si", line)
+	}
 
-    method := parts[0]
-    if method != strings.ToUpper(method) {
-        return nil, fmt.Errorf("Invalid method: %s", method)
-    }
+	method := parts[0]
+	if method != strings.ToUpper(method) {
+		return nil, fmt.Errorf("Invalid method: %s", method)
+	}
 
-    target := parts[1]
+	target := parts[1]
 
-    version_parts := strings.Split(parts[2], "/")
-    if len(version_parts) != 2 {
-        return nil, fmt.Errorf("Malformed version: %s", parts[2])
-    }
+	version_parts := strings.Split(parts[2], "/")
+	if len(version_parts) != 2 {
+		return nil, fmt.Errorf("Malformed version: %s", parts[2])
+	}
 
-    http_part := version_parts[0]
-    if http_part != "HTTP" {
-        return nil, fmt.Errorf("Unsupported protocol: %s", http_part)
-    }
+	http_part := version_parts[0]
+	if http_part != "HTTP" {
+		return nil, fmt.Errorf("Unsupported protocol: %s", http_part)
+	}
 
-    version := version_parts[1]
-    if version != "1.1" {
-        return nil, fmt.Errorf("Unsupported version: %s", version_parts[1])
-    }
+	version := version_parts[1]
+	if version != "1.1" {
+		return nil, fmt.Errorf("Unsupported version: %s", version_parts[1])
+	}
 
-    request_line := RequestLine {
-        HttpVersion: version,
-        RequestTarget: target,
-        Method: method,
-    }
+	request_line := RequestLine{
+		HttpVersion:   version,
+		RequestTarget: target,
+		Method:        method,
+	}
 
-    return &request_line, nil
+	return &request_line, nil
 }
 
 func (r *Request) parse(data []byte) (int, error) {
-    switch r.state {
-    case requestStateInitialized:
-        requestLine, n, err := parseRequestLine(data)
-        if err != nil {
-            return 0, err
-        }
-        if n == 0 {
-            return 0, nil
-        }
-        r.RequestLine = *requestLine
-        r.state = requestStateDone
-        return n, nil
-    case requestStateDone:
-        return 0, fmt.Errorf("error: trying to read but operation is complete")
-    default:
-        return 0, fmt.Errorf("unknown state")
-    }
+	totalBytesParsed := 0
+	// Loop through and make sure we process all complete headers.  If
+	// we don't do that here, the main loop could get an EOF while we
+	// still have unprocessed data in our buffer.
+	for r.state != requestStateDone {
+		n, err := r.parseSingle(data[totalBytesParsed:])
+		if err != nil {
+			return 0, err
+		}
+		totalBytesParsed += n
+		if n == 0 {
+			break
+		}
+	}
+	return totalBytesParsed, nil
 }
 
+func (r *Request) parseSingle(data []byte) (int, error) {
+	switch r.state {
+	case requestStateInitialized:
+		requestLine, n, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if n == 0 {
+			return 0, nil
+		}
+		r.RequestLine = *requestLine
+		r.state = requestStateParsingHeaders
+		return n, nil
+	case requestStateParsingHeaders:
+		log.Printf("Parsing data: [%s]", string(data))
+		n, done, err := r.Headers.Parse(data)
+		if err != nil {
+			return 0, err
+		} else if done {
+			r.state = requestStateDone
+		}
+		return n, nil
+	case requestStateDone:
+		return 0, fmt.Errorf("error: trying to read but operation is complete")
+	default:
+		return 0, fmt.Errorf("unknown state")
+	}
+}
